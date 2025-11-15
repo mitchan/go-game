d8b6e3a5e4ffad123074aa1070d523cac86d1330
Francesco Casetta <francesco.casetta@gmail.com>
2025-11-15 11:31:12 +0100

    refactor: rename Vector to Vector2D and enhance skeleton animations
    - Renamed math.Vector to math.Vector2D for clarity
    - Updated all Draw() method signatures to use Vector2D
    - Enhanced Skeleton entity with animation system (idle, up, down, left, right)
    - Refactored Skeleton movement to use velocity vector
    - Added horizontal flip support for left-facing animation

---
+++ .claude/commands/check-files.md
@@ -0,0 +1,5 @@
+## Task
+
+- controlla le modifiche usando git, alla ricerca di potenziali problemi
+- in caso di problemi, segnalali e fermati
+- se non ci sono problemi, restituisci un breve messaggio di commit
--- entity/skeleton.go
+++ entity/skeleton.go
@@ -1,11 +1,11 @@
 package entity
 
 import (
-	"image"
-
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/mitchan/go-game/animation"
 	"github.com/mitchan/go-game/constants"
 	"github.com/mitchan/go-game/math"
+	"github.com/mitchan/go-game/spritesheet"
 )
 
 type Skeleton struct {
@@ -13,6 +13,11 @@ type Skeleton struct {
 	health        float64
 	followsPlayer bool
 	speed         float64
+	velocity      math.Vector2D
+
+	// animation
+	playerSpriteSheet *spritesheet.SpriteSheet
+	animations        map[entityState]*animation.Animation
 }
 
 func NewSkeleton(img *ebiten.Image, x, y float64) *Skeleton {
@@ -22,38 +27,83 @@ func NewSkeleton(img *ebiten.Image, x, y float64) *Skeleton {
 			X:     x,
 			Y:     y,
 		},
-		health:        100,
-		speed:         1.0,
+		health: 100,
+		speed:  0.5,
+		velocity: math.Vector2D{
+			X: 0,
+			Y: 0,
+		},
 		followsPlayer: true,
+		playerSpriteSheet: &spritesheet.SpriteSheet{
+			WidthInTiles:  6,
+			HeightInTiles: 1,
+			TileSize:      32,
+		},
+		animations: map[entityState]*animation.Animation{
+			idle:  animation.NewAnimation(0, 5, 1, 5.0, false, false),
+			up:    animation.NewAnimation(18, 23, 1, 5.0, false, false),
+			right: animation.NewAnimation(24, 29, 1, 5.0, false, false),
+			left:  animation.NewAnimation(24, 29, 1, 5.0, true, false),
+			down:  animation.NewAnimation(30, 35, 1, 5.0, false, false),
+		},
 	}
 }
 
-func (e *Skeleton) Draw(screen *ebiten.Image, camera math.Vector) {
+func (s *Skeleton) Draw(screen *ebiten.Image, camera math.Vector2D) {
 	opts := ebiten.DrawImageOptions{}
-	opts.GeoM.Translate(e.X, e.Y)
+
+	activeAnimation := s.activeAnimation(s.velocity)
+	imageRect := s.playerSpriteSheet.Rect(activeAnimation.Frame())
+
+	if activeAnimation.FlipH {
+		opts.GeoM.Scale(-1, 1)
+		opts.GeoM.Translate(constants.CellSize, 0)
+	}
+
+	opts.GeoM.Translate(s.X, s.Y)
 	opts.GeoM.Translate(camera.X, camera.Y)
-	screen.DrawImage(
-		e.Image.SubImage(
-			image.Rect(0, 0, constants.CellSize, constants.CellSize),
-		).(*ebiten.Image),
-		&opts,
-	)
+	screen.DrawImage(s.Image.SubImage(imageRect).(*ebiten.Image), &opts)
 }
 
-func (e *Skeleton) Update(player Player) {
-	if !e.followsPlayer {
+func (s *Skeleton) Update(player Player) {
+	s.velocity.X = 0
+	s.velocity.Y = 0
+
+	if !s.followsPlayer {
 		return
 	}
 
-	if player.X > e.X {
-		e.X += e.speed
-	} else if player.X < e.X {
-		e.X -= e.speed
+	if player.X > s.X {
+		s.velocity.X = 1
+	} else if player.X < s.X {
+		s.velocity.X = -1
 	}
 
-	if player.Y > e.Y {
-		e.Y += e.speed
-	} else if player.Y < e.Y {
-		e.Y -= e.speed
+	if player.Y > s.Y {
+		s.velocity.Y = 1
+	} else if player.Y < s.Y {
+		s.velocity.Y = -1
 	}
+
+	s.Sprite.X += s.velocity.X * s.speed
+	s.Sprite.Y += s.velocity.Y * s.speed
+
+	s.activeAnimation(s.velocity).Update()
+}
+
+func (s *Skeleton) activeAnimation(velocity math.Vector2D) *animation.Animation {
+	if velocity.X > 0 {
+		return s.animations[right]
+	}
+	if velocity.X < 0 {
+		return s.animations[left]
+	}
+	if velocity.Y > 0 {
+		return s.animations[up]
+	}
+	if velocity.Y < 0 {
+		return s.animations[down]
+	}
+
+	return s.animations[idle]
 }
